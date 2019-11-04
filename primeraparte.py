061bd58fcabec9ccb120e58083ed0d763e66bad1
geiser pezo <G.B.P.M.2000@gmail.com>
2019-11-04 10:07:07 -0500

    primeraparte

--- primeraparte.py
+++ primeraparte.py
@@ -1,22 +1,22 @@
 import os
 os.system("cls")
 
-print("¡BIENVENIDO A 'AGILIZA TU MENTE'!")
-print("Este juego matemático pone en desafío tu capacidad de resolver operaciones combinadas en el menor tiempo posible.")
+print("BIENVENIDO A 'AGILIZA TU MENTE'!")
+print("Este es un juego matemático donde se pone en desafio tu capacidad de resolver operaciones combinadas en el menor tiempo posible.")
 
 from time import sleep
 for i in range(0,5):
     print(5-i)
     sleep(1)
 while True:
-    de=input("¿Desea jugar?:  (si) (no)")
+    de=input("¿Desea jugar?:  (si) (no) =")
     if de == "no":
-        print("Vuelva pronto")
+        print("Vuelva pronto :-)")
         break
     else:
         print("Resuelva las operaciones en el menor tiempo posible")
         
-        print("Comienza el juego")
+        print("Comienza el juego ")
 
         from time import sleep
         for i in range(0,5):
@@ -31,24 +31,39 @@ while True:
         jugar= "si"
         while jugar=="si":
             inicio=time.time()
-            numazar=random.randint(1,15)
-            numazar1=random.randint(1,15)
-            respuesta=int(input("¿Cuanto es 2*"+str(numazar)+"+"+str(numazar1)+"-2"+"  ?"))
-            final=time.time()
-            tiempo=round(final-inicio,0)
-            if respuesta == numazar*2+numazar1-2:
-                print("Felicidades, resultado correcto",end=" ")
-                if tiempo < 7:
-                    print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡Excelente!")
-                if tiempo >= 7 and tiempo < 16:
-                    print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡lo puedes hacer mejor!")
-                if tiempo >= 16:
-                    print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
+            numazar=random.randint(1,10)
+            numazar1=random.randint(1,10)
+            numazar2=random.randint(1,10)
+            r=int(input("¿Cuanto es: "+str(numazar2)+"*"+str(numazar)+"+"+str(numazar1)+ " ?"))
+            r1=numazar2*numazar+numazar1
+    
+            if r == r1:
+                print("resouesta correcta")
             else:
-                print("Respuesta incorrecta :( Suerte para la próxima")
-                while True:
-                    jugar=input("¿Quieres jugar otra vez? (si)(no) ")
-                    if jugar == "no":
-                        print("Hasta luego ;-) ")    
-                    break
-    break
+                print("respuesta incorrecta")
+            numazar=random.randint(1,10)
+            numazar1=random.randint(1,10)
+            numazar2=random.randint(1,10)
+            z=int(input("¿Cuanto es: "+str(numazar2)+"+"+str(numazar)+"*"+str(numazar1)+ " ?"))
+    
+            z1=numazar2+numazar*numazar1
+
+            if z == z1:
+                print("resouesta correcta")
+              
+            else:
+                print("respuesta incorrecta")
+
+            numazar=random.randint(1,10)
+            numazar1=random.randint(1,10)
+            numazar2=random.randint(1,10)
+            t=int(input("¿Cuanto es: "+str(numazar2)+"-"+str(numazar)+"+"+str(numazar1)+ " ?"))
+
+            t1=numazar2-numazar+numazar1
+            if t == t1:
+                 print("resouesta correcta")
+          
+            else:
+                print("respuesta incorrecta")
+                final=time.time()
+            tiempo=round(final-inicio,0)
