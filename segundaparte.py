3f6c2d0cc0508f1098b6db4ee52bc02233bb2fbb
Mayte Cervantes <dylams.cervantes@gmail.com>
2019-11-03 22:56:11 -0500

    1era versión

---
+++ segundaparte.py
@@ -0,0 +1,16 @@
+
+print("2do Nivel")
+print("¿Cuánto es...?")
+
+a = (15 + 5)*2
+
+b = input("15 + 5 x 2 = ")
+
+if b == 25:
+    print(a, "Respuesta correcta")
+else: 
+    print("Respuesta incorrecta, perdiste un punto")
+
+#print("3er Nivel")
+#print("4to Nivel")
+#print("Game Over")
