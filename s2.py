02829fe869f0d562bdc190e3b6c42fd44e62117c
Mayte Cervantes <dylams.cervantes@gmail.com>
2019-11-04 10:17:59 -0500

    1.0

---
+++ s2.py
@@ -0,0 +1,20 @@
+
+import random
+
+print("2do Nivel")
+b = ["*","+","-"]
+b = random.randint(0,2)
+c = b
+
+for a in range (0,6):
+    numazar = random.randint(1,10)
+    numazar1 = random.randint(1,10)
+    numazar2 = random.randint(1,10)
+    r = int(input("¿Cuánto es: "+str(numazar2)+ c + +str(numazar)+ c +str(numazar1)+ "?"))
+
+    
+    import random
+    y = random.randint(0,2)
+
+    print("Next Level")
+
