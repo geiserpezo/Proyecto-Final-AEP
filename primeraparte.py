7f6a470a2c298f62f4aeec2d31353bea0fada5e3
alumno informatica <alumno@ciis.usil.edu.pe>
2019-11-04 08:20:29 -0500

    version 4.0

--- primeraparte.py
+++ primeraparte.py
@@ -2,52 +2,55 @@ import os
 os.system("cls")
 
 
-
-
-from time import sleep
-for i in range(0,5):
-    print(5-i)
-    sleep(1)
-print("instrucciones")
-print("---------------")
-
-
-print("Este juego estaba basado en resolver operaciones combinadas el la cual saldran aleatoriamente; y a su ves depedera de la habilidad mental para resolverlos e ingresar el resultado")
-
-
-print("Comienza el juego")
+print("Agiliza tu mente es un juego matematico donde se pone en desafio tu capacidad de resolver operaciones combinadas en el menor tiempo posible")
 
 from time import sleep
 for i in range(0,5):
     print(5-i)
     sleep(1)
-
-
-import time
-import random
-
-print("Responde lo mas rapido posible")
-jugar= "si"
-while jugar=="si":
-    inicio=time.time()
-    numazar=random.randint(1,15)
-    numazar1=random.randint(1,15)
-    respuesta=int(input("¿Cuanto es 2*"+str(numazar)+"+"+str(numazar1)+"-2+"+"  ?"))
-    final=time.time()
-    tiempo=round(final-inicio,0)
-    if respuesta == numazar*2+numazar1-2:
-        print("Felicidades, resultado correcto",end=" ")
-        if tiempo < 7:
-            print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡Excelente!")
-        if tiempo >= 7 and tiempo < 16:
-            print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡lo puedes hacer mejor!")
-        if tiempo >= 16:
-            print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
+while True:
+    de=input("¿Desea jugar?  (si)(no)")
+    if de == "no":
+        print("Vuelva pronto")
+        break
     else:
-        print("Suerte para la proxima")
-        jugar=input("¿Quieres jugar otra vez? (si)(no)   ")
-        if jugar == "NO":
-            break
-print(" Hasta luego ;-)  ")
+        print("Resuelva las operaciones en el menor tiempo posible")
+        
+        print("Comienza el juego")
+
+        from time import sleep
+        for i in range(0,5):
+            print(5-i)
+            sleep(1)
+
+
+        import time
+        import random
+
+        print("Responda lo mas rapido posible")
+        jugar= "si"
+        while jugar=="si":
+            inicio=time.time()
+            numazar=random.randint(1,15)
+            numazar1=random.randint(1,15)
+            respuesta=int(input("¿Cuanto es 2*"+str(numazar)+"+"+str(numazar1)+"-2+"+"  ?"))
+            final=time.time()
+            tiempo=round(final-inicio,0)
+            if respuesta == numazar*2+numazar1-2:
+                print("Felicidades, resultado correcto",end=" ")
+                if tiempo < 7:
+                    print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡Excelente!")
+                if tiempo >= 7 and tiempo < 16:
+                    print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡lo puedes hacer mejor!")
+                if tiempo >= 16:
+                    print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
+            else:
+                print("Suerte para la proxima")
+                while True:
+                    jugar=input("¿Quieres jugar otra vez? (si)(no)   ")
+                    if jugar == "no":
+                        break
+                print(" Hasta luego ;-)  ")
+    break
 
 
