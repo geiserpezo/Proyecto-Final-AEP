af559d733fc0188f1f30f1d04b05e440fa3dc8cb
geiser pezo <G.B.P.M.2000@gmail.com>
2019-11-04 09:33:27 -0500

    primeraparte

--- primeraparte.py
+++ primeraparte.py
@@ -2,14 +2,14 @@ import os
 os.system("cls")
 
 print("BIENVENIDO A 'AGILIZA TU MENTE'!")
-print("Este un juego matemático donde se pone en desafio tu capacidad de resolver operaciones combinadas en el menor tiempo posible.")
+print("Este es un juego matemático donde se pone en desafio tu capacidad de resolver operaciones combinadas en el menor tiempo posible.")
 
 from time import sleep
 for i in range(0,5):
     print(5-i)
     sleep(1)
 while True:
-    de=input("¿Desea jugar?:  (si)(no)")
+    de=input("¿Desea jugar?:  (si) (no)")
     if de == "no":
         print("Vuelva pronto")
         break
@@ -33,7 +33,7 @@ while True:
             inicio=time.time()
             numazar=random.randint(1,15)
             numazar1=random.randint(1,15)
-            respuesta=int(input("¿Cuanto es 2*"+str(numazar)+"+"+str(numazar1)+"-2+"+"  ?"))
+            respuesta=int(input("¿Cuanto es 2*"+str(numazar)+"+"+str(numazar1)+"-2"+"  ?"))
             final=time.time()
             tiempo=round(final-inicio,0)
             if respuesta == numazar*2+numazar1-2:
@@ -45,9 +45,9 @@ while True:
                 if tiempo >= 16:
                     print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
             else:
-                print("Suerte para la proxima")
+                print("Respuesta incorrecta :( Suerte para la proxima")
                 while True:
-                    jugar=input("¿Quieres jugar otra vez? (si)(no)   ")
+                    jugar=input("¿Quieres jugar otra vez? (si)(no) ")
                     if jugar == "no":
                         print(" Hasta luego ;-)  ")    
                     break
