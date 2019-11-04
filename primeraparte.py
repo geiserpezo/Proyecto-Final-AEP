cba20e11c1b22d71f6d85ba95d7c06cacb3a03b0
Mayte Cervantes <dylams.cervantes@gmail.com>
2019-11-04 08:42:03 -0500

    5.0

--- primeraparte.py
+++ primeraparte.py
@@ -1,16 +1,16 @@
 import os
 os.system("cls")
 
-
-print("Agiliza tu mente es un juego matematico donde se pone en desafio tu capacidad de resolver operaciones combinadas en el menor tiempo posible")
+print("BIENVENIDO A 'AGILIZA TU MENTE'!)
+print("Este un juego matemático donde se pone en desafio tu capacidad de resolver operaciones combinadas en el menor tiempo posible.")
 
 from time import sleep
 for i in range(0,5):
     print(5-i)
     sleep(1)
 while True:
-    de=input("¿Desea jugar?  (si)(no)")
-    if de == "no":
+    de=input("¿Desea jugar?:  (Si)(No)")
+    if de == "No":
         print("Vuelva pronto")
         break
     else:
@@ -27,7 +27,7 @@ while True:
         import time
         import random
 
-        print("Responda lo mas rapido posible")
+        print("Responda lo mas rápido posible")
         jugar= "si"
         while jugar=="si":
             inicio=time.time()
