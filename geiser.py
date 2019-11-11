809f7292e9a3e3e09a25480b30c1e31f09be820b
geiser pezo <G.B.P.M.2000@gmail.com>
2019-11-10 23:31:00 -0500

    geiser

---
+++ geiser.py
@@ -0,0 +1,53 @@
+import os
+os.system("cls")
+
+print("BIENVENIDO A 'AGILIZA TU MENTE'!")
+print("Este es un juego matemático donde se pone en desafio tu capacidad de resolver operaciones combinadas en el menor tiempo posible.")
+
+
+from time import sleep
+for i in range(0,5):
+    print(5-i)
+    sleep(1)
+
+while True:
+    de=input("¿Desea jugar?:  (si) (no) =")
+    if de == "no":
+        print("Vuelva pronto :-)")
+        break
+    else:
+        print("Resuelva las operaciones en el menor tiempo posible")
+        
+        print("Comienza el juego ")
+
+        from time import sleep
+        for i in range(0,3):
+            print(3-i)
+            sleep(1)
+
+        import time
+        import random
+
+        print("Responde lo mas rapido posible")
+        jugar= "si"
+        while jugar=="si":
+            inicio=time.time()
+            numazar=random.randint(1,15)
+            numazar1=random.randint(1,15)
+            respuesta=int(input("¿Cuanto es:"+str(numazar)+"+2"+"+"+str(numazar1)+"-5 ?=   "))
+            final=time.time()
+            tiempo=round(final-inicio,0)
+            if respuesta == numazar+2+numazar1-5:
+                print("Felicidades, resultado correcto",end=" ")
+                if tiempo < 7:
+                    print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡Excelente!")
+                if tiempo >= 7 and tiempo < 16:
+                    print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡lo puedes hacer mejor!")
+                if tiempo >= 16:
+                    print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
+            else:
+                print("Suerte para la proxima")
+                jugar=input("¿Quieres jugar otra vez? (si)(no)   ")
+                if jugar == "NO":
+                    break
+    print(" Hasta luego ;-)  ")
