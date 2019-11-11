1c17376f952b9ea1662c23c8e62727af96d162a9
unknown <nicolas021098@gmail.com>
2019-11-11 04:26:58 -0500

    casi final

--- geiser.py
+++ geiser.py
@@ -51,9 +51,8 @@ while True:
             else:
                 print("Suerte para la proxima")
                 jugar=input("¿Quieres jugar otra vez? (si)(no)   ")
-                if jugar == "NO":
+                if jugar == "no":
                     break
-                break
             
     
             numazar=random.randint(1,25)
@@ -76,9 +75,8 @@ while True:
             else:
                 print("Suerte para la proxima")
                 jugar=input("¿Quieres jugar otra vez? (si)(no)   ")
-                if jugar == "NO":
+                if jugar == "no":
                     break
-                break
            
 
             numazar=random.randint(1,25)
@@ -100,11 +98,10 @@ while True:
             else:
                 print("Suerte para la proxima")
                 jugar=input("¿Quieres jugar otra vez? (si)(no)   ")
-                if jugar == "NO":
+                if jugar == "no":
                     break
-                break
 
-
+            import time
             import random
 
             print("Pasaste al Segundo nivel")
@@ -115,51 +112,77 @@ while True:
                 sleep(1)
 
             for a in range(0,2):
+                inicio=time.time()
 
                 numazar=random.randint(1,10)
                 numazar1=random.randint(1,10)
                 numazar2=random.randint(1,10)
                 r=float(input("¿ Cuánto es: "+str(numazar2)+"*"+str(numazar)+"+"+str(numazar1)+ " ? = "))
+                final=time.time()
+                tiempo=round(final-inicio,0)
                 r1=numazar2*numazar+numazar1
 
                 if r == r1:
                     print("Respuesta correcta")
+                    if tiempo < 7:
+                        print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡Excelente!")
+                    if tiempo >= 7 and tiempo < 16:
+                        print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡lo puedes hacer mejor!")
+                    if tiempo >= 16:
+                        print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
                 else:
                     print("Respuesta incorrecta")
                     print("GAME OVER")
                     break
-                break
+               
 
                 numazar=random.randint(1,10)
                 numazar1=random.randint(1,10)
                 numazar2=random.randint(1,10)
                 z=int(input("¿ Cuánto es: "+str(numazar2)+"+"+str(numazar)+"-"+str(numazar1)+ " ? = "))
-
+                final=time.time()
+                tiempo=round(final-inicio,0)
+    
                 z1=numazar2+numazar-numazar1
 
                 if z == z1:
                     print("Respuesta correcta")
+                    if tiempo < 7:
+                        print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡Excelente!")
+                    if tiempo >= 7 and tiempo < 16:
+                        print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡lo puedes hacer mejor!")
+                    if tiempo >= 16:
+                        print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
                     
                 else:
                     print("Respuesta incorrecta")
                     print("GAME OVER ")
                     break
-                break
+               
 
                 numazar=random.randint(1,10)
                 numazar1=random.randint(1,10)
                 numazar2=random.randint(1,10)
                 t=int(input("¿ Cuánto es: "+str(numazar2)+"-"+str(numazar)+"*"+str(numazar1)+ " ? = "))
-
+                final=time.time()
+                tiempo=round(final-inicio,0)
                 t1=numazar2-numazar*numazar1
                 if t == t1:
                     print("Respuesta correcta")
-                    print("NEXT LEVEL")
+                    if tiempo < 7:
+                        print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡Excelente!")
+                    if tiempo >= 7 and tiempo < 16:
+                        print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡lo puedes hacer mejor!")
+                    if tiempo >= 16:
+                        print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
+
                 else:
                     print("Respuesta incorrecta")
                     break
-                break
-
+               
+                
+                print("Next level")
+                import time
                 import random
 
                 print("Pasaste al tercer nivel")
@@ -170,52 +193,79 @@ while True:
                     sleep(1)
 
                 for a in range(0,2):
+                    inicio=time.time()
 
                     numazar=random.randint(1,10)
                     numazar1=random.randint(1,10)
                     numazar2=2
                     numazar3=random.randint(1,10)
                     r=float(input("¿ Cuánto es: "+str(numazar)+"-"+str(numazar1)+"/"+str(numazar2)+"+"+str(numazar3)+ " ? = "))
+                    final=time.time()
+                    tiempo=round(final-inicio,0)
                     r1=numazar-numazar1/numazar2+numazar3
 
                     if r == r1:
                         print("Respuesta correcta")
+                        if tiempo < 7:
+                            print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡Excelente!")
+                        if tiempo >= 7 and tiempo < 16:
+                            print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡lo puedes hacer mejor!")
+                        if tiempo >= 16:
+                             print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
+       
                     else:
                         print("Respuesta incorrecta")
                         print("GAME OVER")
                         break
-                    break
+                  
 
                     numazar=random.randint(1,10)
                     numazar1=random.randint(1,10)
                     numazar2=random.randint(1,10)
                     numazar3=4
                     z=float(input("¿ Cuánto es: "+str(numazar)+"-"+str(numazar1)+"*"+str(numazar2)+"/"+str(numazar3)+ " ? = "))
-
+                    final=time.time()
+                    tiempo=round(final-inicio,0)
                     z1=numazar-numazar1*numazar2/numazar3
 
                     if z == z1:
                         print("Respuesta correcta")
+                        if tiempo < 7:
+                             print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡Excelente!")
+                        if tiempo >= 7 and tiempo < 16:
+                             print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡lo puedes hacer mejor!")
+                        if tiempo >= 16:
+                             print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
+       
                         
                     else:
                         print("Respuesta incorrecta")
                         print("GAME OVER ")
                         break
-                    break
+            
+                   
 
                     numazar=random.randint(1,10)
                     numazar1=random.randint(1,10)
                     numazar2=random.randint(1,10)
                     numazar3=random.randint(1,10)
                     t=float(input("¿ Cuánto es: "+str(numazar)+"*"+str(numazar1)+"+"+str(numazar2)+"-"+str(numazar3)+ " ? = "))
-
+                    final=time.time()
+                    tiempo=round(final-inicio,0)
                     t1=numazar*numazar1+numazar2-numazar3
                     if t == t1:
                         print("Respuesta correcta")
+                        if tiempo < 7:
+                            print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡Excelente!")
+                        if tiempo >= 7 and tiempo < 16:
+                            print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡lo puedes hacer mejor!")
+                        if tiempo >= 16:
+                            print("tu tiempo fue de:   " +str(tiempo)+"   segundos, ¡te falta practicar velocidad mental, pero vamos bien!")
+       
                         print("Ganaste el juego, mil de IQ")
                     else:
                         print("Respuesta incorrecta")
                         break
-                    break
+        
            
     print(" Hasta luego, regresa pronto ;-)  ")
